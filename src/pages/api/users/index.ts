fd652e85cc7cacae100d5a8e37f427b3e8e397e6
Emmanuel Villegas Gonzalez <emmanuelvillegasgonzalez@gmail.com>
2020-10-28 10:54:12 -0400

    add prettier and format

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,13 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+
 import { sampleUserData } from '../../../utils/sample-data';
 
-const handler = (_req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (
+	_req: NextApiRequest,
+	res: NextApiResponse
+): Promise<void> => {
+	const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+	const posts = await data.json();
 	try {
 		if (!Array.isArray(sampleUserData)) {
 			throw new Error('Cannot find user data');
 		}
 
-		res.status(200).json(sampleUserData);
+		res.status(200).json(posts);
 	} catch (err) {
 		res.status(500).json({ statusCode: 500, message: err.message });
 	}
