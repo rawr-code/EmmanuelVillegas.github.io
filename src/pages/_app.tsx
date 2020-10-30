003d6f978b18fa92b7d93648be174e8214556630
Emmanuel Villegas Gonzalez <emmanuelvillegasgonzalez@gmail.com>
2020-10-30 19:11:04 -0400

    add font Poppings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,11 @@ const MainApp = ({ Component, pageProps }: AppProps): React.ReactElement => {
 					name="viewport"
 					content="initial-scale=1.0, width=device-width"
 				/>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900"
+					type="text/css"
+				/>
 			</Head>
 			<ThemeProvider theme={theme}>
 				<GlobalStyles />
