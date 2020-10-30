334e032d98735eeb7b8919322f08622e742244a0
Emmanuel Villegas Gonzalez <emmanuelvillegasgonzalez@gmail.com>
2020-10-30 19:10:41 -0400

    update global styles

--- src/theme/GlobalStyles.ts
+++ src/theme/GlobalStyles.ts
@@ -1,15 +1,28 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  *, ::after, ::before {
+    box-sizing: border-box;
+  }
+
+  html {
+    -webkit-tap-highlight-color: transparent;
+  }
+
   body {
     margin: 0;
     padding: 0;
-
-	h1,h2,h3,h4,h5,h6,p {
-		margin: 0;
-    	padding: 0;
-	}
+    font-family: "Poppins",sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    background-color: #212529;
   }
+
+	h1, h2, h3, h4, h5, h6 {
+		margin: 0;
+    padding: 0;
+	}
 `;
 
 export default GlobalStyle;
